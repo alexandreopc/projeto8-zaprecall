6371639aea6ed6ee82f9ee0d1380fc4549949592
Alexandre Costa <alexandreodpc@gmail.com>
2022-03-19 13:10:23 -0300

    esqueci de fazer commit :)

--- src/components/Carta.jsx
+++ src/components/Carta.jsx
@@ -1,20 +1,50 @@
 import { useState } from "react";
 
 export default function Carta(props) {
-    const [flashAtivo, setFlashAtivo] = useState(false);
+    const [flashSelecionado, setFlashSelecionado] = useState(false);
+    const [flashVirado, setFlashVirado] = useState(false);
     const [resposta, setResposta] = useState("");
-    return flashAtivo?(
-        <div className="carta">
-            <p>{props.flash}</p>
-            <div className="botoes">
-                <button onClick={() => setResposta("")}>Não lembrei</button>
-                <button>Quase não lembrei</button>
-                <button>Zap!</button>
+    if(flashVirado){
+        return (
+            <div className="carta">
+                <p>{props.flash}</p>
+                <div className="botoes">
+                    <button>Não lembrei</button>
+                    <button>Quase não lembrei</button>
+                    <button>Zap!</button>
+                </div>
+            </div> 
+        );
+    }
+    if(!flashSelecionado){
+        return (
+            <div className="carta" onClick={() => setFlashSelecionado(true)}>
+                <p>Pergunta {props.index+1}</p>
             </div>
-        </div>
-    ) : (
-        <div className="carta" onClick={() => setFlashAtivo(true)}>
-            <p>Pergunta {props.index+1}</p>
-        </div>
-    )
+        );
+    }
+    if(flashSelecionado){
+        return (
+            <div className="carta">
+                <p>{props.flash}</p>
+                <div onClick={()=> setFlashVirado(true)}>
+                    <p>botao</p>
+                </div>
+            </div>
+        );
+    }
+//     return flashSelecionado?(
+//         <div className="carta">
+//             <p>{props.flash}</p>
+//             <div className="botoes">
+//                 <button onClick={() => setResposta("")}>Não lembrei</button>
+//                 <button>Quase não lembrei</button>
+//                 <button>Zap!</button>
+//             </div>
+//         </div>
+//     ) : (
+//         <div className="carta" onClick={() => setFlashSelecionado(true)}>
+//             <p>Pergunta {props.index+1}</p>
+//         </div>
+//     )
 }
