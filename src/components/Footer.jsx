49eee9ca84acfe28b1be3236784d41121499ef66
Alexandre Costa <alexandreodpc@gmail.com>
2022-03-21 15:30:22 -0300

    TERMINEI

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -28,9 +28,14 @@ export default function Footer(props){
         if(aux === 1){
         return (
             <div className="footer finalizado">
+                <div className="mensagem">
+                    <img src="./assets/sad.png" alt="sad"/>
+                    <p>Putz...</p>
+                </div>
+                <p>Ainda faltam alguns...</p>
+                <p>Mas não desanime!</p>
                 <p>{props.flashRespondidos}/8 CONCLUÍDOS</p>
                 <div className="respostas">
-                    <img src="./../public/assets/sad.png" alt="sad"/>
                     {props.respostas.map((resposta)=> {
                         if(resposta === "carta incorreta"){
                             return <ion-icon class="wrong-icon md hydrated" name="close-circle"></ion-icon>
@@ -45,7 +50,26 @@ export default function Footer(props){
             );
         }else{
             return (
-                <></>
+                <div className="footer finalizado">
+                <div className="mensagem">
+                    <img src="./assets/party.png" alt="party"/>                    
+                    <p>Parabéns!</p>
+                </div>
+                <p>Você não esqueceu de </p>
+                <p>nenhum flashcard!</p>
+                <p>{props.flashRespondidos}/8 CONCLUÍDOS</p>
+                <div className="respostas">
+                    {props.respostas.map((resposta)=> {
+                        if(resposta === "carta incorreta"){
+                            return <ion-icon class="wrong-icon md hydrated" name="close-circle"></ion-icon>
+                        }
+                        if(resposta === "carta quase-correta")
+                            return <ion-icon class="in-between-icon md hydrated" name="help-circle"></ion-icon>
+                        if(resposta === "carta correta")
+                            return <ion-icon class="success-icon md hydrated" name="checkmark-circle"></ion-icon>
+                    })}
+                </div>
+            </div>
             );
         }
 
